91f21922e2e7d2c41b8dad24e0ce0dc58ed80c3b
Xander Schils <xander.schils@student.howest.be>
2026-05-11 16:01:38 +0200

    added some more pics

--- assets/js/data/gallery-images.js
+++ assets/js/data/gallery-images.js
@@ -6,12 +6,12 @@ window.pageImages = [
     },
     {
         src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443280/Snapchat-390492079_xr5jie.jpg',
-        alt: 'Handmade Microphone',
+        alt: 'Campfire',
         caption: 'Upcoming Artist'
     },
     {
         src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443278/IMG-20260501-WA0015_o6rifz.jpg',
-        alt: 'Beer',
+        alt: 'Nightlife',
         caption: 'Is it a bird, A plane? Or just some beer'
     },
     {
@@ -28,5 +28,65 @@ window.pageImages = [
         src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443282/IMG-20260508-WA0007_ijbmuc.jpg',
         alt: 'Nightlife',
         caption: 'The start of a meter tall'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443234/IMG_20260509_225014_rfud8m.jpg',
+        alt: 'Campfire',
+        caption: 'Some models are needed'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443229/IMG_20260509_224934_qxmpwx.jpg',
+        alt: 'Campfire',
+        caption: 'Representing the Swedish'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/ar_4:3,c_auto,g_north/IMG_20260509_223600_1_glmnmi.jpg',
+        alt: 'Campfire',
+        caption: 'Is that our professional Chinese needle?'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443124/IMG_20260509_190132_1_awdi7c.jpg',
+        alt: 'Park',
+        caption: 'Guess the temperature'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443079/IMG_20260507_193814_1_ymqfdv.jpg',
+        alt: 'Hostel',
+        caption: 'What sidequest do we get today?'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443075/IMG_20260507_193720_1_cavhsn.jpg',
+        alt: 'Hostel',
+        caption: 'Is there a storm nearby?'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443069/eee36f8c-0380-4d66-8f1a-b60148bb741f_xvcdeq.jpg',
+        alt: 'Nightlife',
+        caption: 'More money wasted'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443072/IMG_20260508_132102_375_ib3sjj.jpg',
+        alt: 'Hostel',
+        caption: 'Namaste my friend'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443070/IMG_20260507_193621_cxsmte.jpg',
+        alt: 'Hostel',
+        caption: 'The Lorax'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443068/IMG_20260507_193649_hv7j8b.jpg',
+        alt: 'Hostel',
+        caption: 'Do I go bald? Yes that would look better. But Seriously, do I go bald?'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443062/IMG_20260504_203355_aaed3h.jpg',
+        alt: 'Boardgames',
+        caption: 'My _ just burst'
+    },
+    {
+        src: 'https://res.cloudinary.com/dtncz9cld/image/upload/v1778443058/IMG_20260507_193600_ravk7o.jpg',
+        alt: 'Hostel',
+        caption: 'What was the record again? 40?'
     }
 ];
